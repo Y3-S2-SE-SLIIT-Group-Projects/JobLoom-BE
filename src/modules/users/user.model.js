6fe82b65203faa739dd00fcbdcff405a9926e2a7
Rasanga Bawanthi <rasanga@cloudcostume.co.uk>
2026-02-23 22:04:33 +0530

    Revert "feat(api): sms service config #0"
    This reverts commit c7fac9943b05518e7e7d475caae6d3341bbf355e.

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -1,6 +1,30 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
+const ratingStatsSchema = new mongoose.Schema(
+  {
+    averageRating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    totalReviews: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    ratingDistribution: {
+      5: { type: Number, default: 0 },
+      4: { type: Number, default: 0 },
+      3: { type: Number, default: 0 },
+      2: { type: Number, default: 0 },
+      1: { type: Number, default: 0 },
+    },
+  },
+  { _id: false }
+);
+
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -62,14 +86,6 @@ const userSchema = new mongoose.Schema(
     isActive: {
       type: Boolean,
       default: true,
-    },
-    otp: {
-      type: String,
-      default: null,
-    },
-    otpExpiry: {
-      type: Date,
-      default: null,
     },
   },
   {
